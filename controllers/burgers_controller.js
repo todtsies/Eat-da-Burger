9bdf649a349a09b18aa7bb6e2809ef3f98cefd2e
todtsies <emilybtodt@gmail.com>
2021-03-01 11:16:05 -0500

    Update burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -24,9 +24,8 @@ router.put('/api/burgers/:id', function(req, res) {
     var condition = 'id = ' + req.params.id;
     console.log('condition', condition);
     
-    burger.update({
-      devoured: req.body.devoured
-    }, condition, function(result) {
+    burger.update({devoured: req.body.devoured}, condition, function(result) {
+      
       if (result.changedRows == 0) {
         return res.status(404).end();
       } else {
@@ -34,3 +33,18 @@ router.put('/api/burgers/:id', function(req, res) {
       }
     });
   });
+
+  router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    
+    burger.delete(condition, function(result) {
+      
+      if (result.affectedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+
+  module.exports = router;
